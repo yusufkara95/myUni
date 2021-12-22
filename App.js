9394b20b8beea84ba79bd68f1ec156a3db3d0e3f
Yusuf Kara <94810547+yugamuga@users.noreply.github.com>
2021-12-22 19:41:15 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+
+class App extends React.Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default App;
