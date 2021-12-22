ce4b7386bf5262013bf5a9ff6038b8ae72043525
Yusuf Kara <94810547+yugamuga@users.noreply.github.com>
2021-12-22 20:57:05 +0100

    add the input field for the login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState} from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import HomeScreen from './HomeScreen'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <KeyboardAvoidingView style={styles.container} behavior='padding'>
-            <View>
+            <View style={styles.inputContainer}>
                 <TextInput 
                     placeholder='Hochschul-Email' 
                     //value={} 
@@ -19,10 +22,18 @@ const LoginScreen = () => {
                     style={styles.input}
                     secureTextEntry
                 />
-
-                
             </View>
 
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity 
+                    onPress={() => { }}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>
+                        Anmelden
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </KeyboardAvoidingView>
     )
 }
@@ -34,5 +45,33 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    inputContainer: {
+        width: '80%'
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5
+    },
+    buttonContainer: {
+        width: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16
     }
 })
