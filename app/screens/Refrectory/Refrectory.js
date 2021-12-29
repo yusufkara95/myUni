3fd68cb1856861b8790e5e7cce839e23fd8feeee
Yusuf Kara <94810547+yugamuga@users.noreply.github.com>
2021-12-29 17:59:10 +0100

    Moving the folder refrectory in a folder

---
+++ app/screens/Refrectory/Refrectory.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Icon } from "react-native-elements"
+import { firebaseApp } from "../../utils/firebase"
+import firebase from 'firebase/app'
+
+export default function Refrectory() {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((userInfo) => {
+            setUser(userInfo)
+        });
+    })
+
+    return (
+        <View style={styles.viewBody}>
+            <Text>Mensa</Text>
+
+            {user && (
+                <Icon 
+                reverse
+                type="ionicon"
+                name="add"
+                color="#00a2e5"
+                containerStyle={styles.buttonAddContainer}
+            />
+            )}
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1,
+        backgroundColor: "#FFF"
+    }, 
+    buttonAddContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+        shadowColor: "black",
+        shadowOffset: {width: 1, height: 1},
+        shadowOpacity: 0.1
+    }
+})
