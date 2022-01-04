21c9a3df865379edf83f4faf1dbe7613ca328bcd
Yusuf Kara <94810547+yugamuga@users.noreply.github.com>
2022-01-04 14:41:24 +0100

    Finish the code for the detail view for the event screen

--- app/screens/Event/EventDetail.js
+++ app/screens/Event/EventDetail.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import { Rating } from 'react-native-elements';
 import Loading from '../../components/Loading';
 
 import {firebaseApp} from "../../utils/firebase"
 import firebase from 'firebase/app';
 import "firebase/firestore"
+import { Button } from 'react-native-elements';
+
+import ListSubscriber from '../../components/Event/ListSubscriber';
 
 const db =  firebase.firestore(firebaseApp);
 
 export default function EventDetail(props) {
     const { navigation, route } = props;
-    const { id, name } = route.params;
+    const { id, name, idEvent, acceptCount } = route.params;
     const [ event, setEvent] = useState(null);
-    const [ rating, setRating] = useState(0);
+    const [ votingTotal, setVotingTotal] = useState(0);
+    const [ votingAccept, setVotingAccept] = useState(0);
 
     navigation.setOptions({ title: name });
 
@@ -25,50 +28,63 @@ export default function EventDetail(props) {
             const data = response.data();
             data.id = response.id;
             setEvent(data);
-            setRating(data.rating) 
+            setVotingTotal(data.votingTotal);
+            setVotingAccept(data.votingAccept);
         })
     }, [])
 
     if(!event) return <Loading isVisible={true} text="Ladet..." />
 
+
+
     return (
-        <ScrollView vertical style={styles.viewBody}>
+        <ScrollView vertical>
             <TitleEvent 
                 name={event.name}
                 description={event.description}
-                rating={event.rating}
+                votingTotal={event.votingTotal}
+                votingAccept={event.votingAccept}
             />
-
+            <ListSubscriber                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+                navigation={navigation}
+                idEvent={event.id}
+                setVotingAccept={setVotingAccept}
+            />
         </ScrollView>
     )
 }
 
 function TitleEvent(props) {
-    const {name, description, rating} = props;
+    const {name, description, votingTotal, votingAccept, declineEvent} = props;
 
     return (
-        <View style={styles.eventTitle}>
-            <View style={{flexDirection: "row"}}>
+        <View>
+            <View style={styles.imageView}></View>
+        <View style={styles.eventBody}>
+            <View style={{flexDirection: "row", paddingTop: 20, paddingBottom: 20}}>
                 <Text style={styles.nameEvent}>{name}</Text>
-                <Rating 
-                    style={styles.rating} 
-                    imageSize={20}
-                    readonly
-                    startingValue={parseFloat(rating)}
-                />
             </View>
             <Text style={styles.descriptionEvent}>{description}</Text>
+        </View>
+            <Text>{votingAccept} Personen nehmen teil</Text>
+            <Text>Abgestimmte Personen: {votingTotal} </Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    viewBody: {
+    eventView: {
         flex: 1,
-        backgroundColor: "#FFF"
+        backgroundColor: "#FFF",
     },
-    titleEvent: {
-        padding: 15,
+    imageView: {
+        height: 250,
+        //borderBottomRightRadius: 40,
+        //borderBottomLeftRadius: 40,
+        backgroundColor: "#00a2e5"
+    },
+    eventBody: {
+        paddingHorizontal: 20
     },
     nameEvent: {
         fontSize: 20,
@@ -76,10 +92,15 @@ const styles = StyleSheet.create({
     }, 
     descriptionEvent: {
         marginTop: 5,
-        color: "grey"
+        color: "grey",
+        paddingBottom: 20,
     }, 
     rating: {
         position: "absolute",
         right: 0
+    }, 
+    buttonView: {
+        flexDirection: "row",
+        padding: 10
     }
 })
