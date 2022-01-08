16eaee96e50ad79869b5ce0a9e5dfdbad840b4d5
Yusuf Kara <94810547+yugamuga@users.noreply.github.com>
2022-01-08 18:28:15 +0100

    Update Map.js

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,30 +1,34 @@
 import React from "react";
 import MapView from "react-native-maps";
-import openMap from "react-native-open-maps";
+import openMap from "react-native-open-maps"
 
 export default function Map(props) {
 
     const { location, name, height } = props;
+
     const openAppMap = () => {
         openMap({
             latitude: location.latitude,
             longitude: location.longitude,
             zoom: 19,
-        });
-    };
+            query: name
+        })
+    }
+    
 
     return (
         <MapView
-        style={{ height: height, width: "100%" }}
-        initialRegion={location}
-        onPress={openAppMap}
-    >
-        <MapView.Marker
-            coordinate={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-        }}
-        />
+            style={{ height: height, width: "100%" }}
+            initialRegion={location}
+            onPress={openAppMap}
+        >
+            <MapView.Marker 
+                coordinate={{
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                }}
+            />
+
         </MapView>
     );
 }
