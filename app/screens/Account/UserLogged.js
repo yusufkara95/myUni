9bec1b652eecdce3450db73efc8bd19cb750c61f
Yusuf Kara <94810547+yugamuga@users.noreply.github.com>
2022-01-16 17:23:14 +0100

    Update UserLogged.js

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,11 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Button } from "react-native-elements"
+import { Button, Icon } from "react-native-elements"
 import * as firebase from "firebase"
 import Loading from "../../components/Loading"
 import InfoUser from '../../components/Account/InfoUser'
+import { useNavigation } from '@react-navigation/native';
+
+
 
 export default function UserLogged() {
+    const navigation = useNavigation();
     const [userInfo, setUserInfo] = useState({})
     const [loading, setLoading] = useState(false);
     const [loadingText, setLoadingText] = useState("");
@@ -20,13 +24,27 @@ export default function UserLogged() {
     return (
         <View style={styles.viewUserInfo}>
             {userInfo && <InfoUser userInfo={userInfo} />}
-        
+            
             <Button 
                 title='Abmelden'
                 buttonStyle={styles.buttonSignOut}
                 titleStyle={styles.buttonSignOutText}
                 onPress={() => firebase.auth().signOut()}
             />
+            <Button 
+                title='Impressum'
+                buttonStyle={styles.buttonSignOut}
+                titleStyle={styles.buttonSignOutText}
+                onPress={() => navigation.navigate("impressum")}
+            />
+            <View style={styles.meme}>
+                    <Icon
+                        name='bug'
+                        type='ionicon'
+                        color='#FFF'
+                    />
+                <Text style={{color: "#FFF"}}>It's not a bug, it's a feature</Text>
+            </View>
             <Loading text={loadingText} isVisible={loading} />
         </View>
     )
@@ -38,7 +56,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#f2f2f2",
     }, 
     buttonSignOut: {
-        marginTop: 30,
+        marginTop: 10,
         borderRadius: 0,
         backgroundColor: "#FFFFFF",
         borderTopWidth: 1,
@@ -50,5 +68,11 @@ const styles = StyleSheet.create({
     },
     buttonSignOutText: {
         color: "#00a2e5"
+    },
+    meme: {
+        backgroundColor: "#00a2e5",
+        padding: 20,
+        marginTop: 20,
+        alignItems: "center",
     }
 })
